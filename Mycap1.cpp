bae89e6f2ab9c48cf68aa15417d8b4a3a47cf6ec
Namratha2003 <92636518+Namratha2003@users.noreply.github.com>
2021-10-17 21:24:41 +0530

    Program to find size of int, float, char and double in your system

---
+++ Mycap1.cpp
@@ -0,0 +1,11 @@
+//size of int float double and char
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Size of char: "<<sizeof(char)<<" bytes ";
+	cout<<"Size of int: "<<sizeof(int)<<" bytes ";
+	cout<<"Size of float: "<<sizeof(float)<<" bytes ";
+	cout<<"Size of double: "<<sizeof(double)<<" bytes ";
+	
+}
